d2f50ad5173819410499e37d3fb281d4bdcbf608
AbraaoMorais <moraisabraao@gmail.com>
2021-02-09 00:21:06 -0300

    updated the screens wireframes

---
+++ website_1/js/teste.js
@@ -0,0 +1,10 @@
+var tempoViagem = 10;     //tempo gasto no percurso na escala hora
+var velocidadeMedia = 85;     // km/h
+
+
+/*
+function calc(){
+    calc = tempoViagem/velocidadeMedia;
+  
+} */
+console.log(tempoViagem);
