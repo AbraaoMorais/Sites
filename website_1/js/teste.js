a1367bf4d59385d687437ef006c9149deba9c4a4
AbraaoMorais <moraisabraao@gmail.com>
2021-02-12 12:00:14 -0300

    test in the terminal from VsCode

--- website_1/js/teste.js
+++ website_1/js/teste.js
@@ -1,10 +1,5 @@
-var tempoViagem = 10;     //tempo gasto no percurso na escala hora
-var velocidadeMedia = 85;     // km/h
+// var tempoViagem = 10;     //tempo gasto no percurso na escala hora
+// var velocidadeMedia = 85;     // km/h
 
 
-/*
-function calc(){
-    calc = tempoViagem/velocidadeMedia;
-
-} */
-console.log(tempoViagem);
+// console.log(tempoViagem); 
