4b6b6dc553afda6b6c3278b1f9016e50effdcde3
AbraaoMorais <moraisabraao@gmail.com>
2021-04-24 03:28:22 -0300

    seconds changed from 3 to 9s

---
+++ website_2/my-portfoliosite/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from "../styles/footer.module.css"
+
+const footer = () =>{
+    return(
+        <footer className = {styles.footer}>
+            <h1>Coperight © 2021 - Abraão Morais</h1>
+            
+        </footer>
+    )
+}
+
+export default footer
