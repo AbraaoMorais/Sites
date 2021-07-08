0329021a5087d7430bfcf76a1974b0eabd62a5b5
Abraao morais santos <abraao.santos@rocky.ag>
2021-07-08 01:14:15 -0300

    updated modules

--- website_2/my-portfoliosite/components/SwithPage.js
+++ website_2/my-portfoliosite/components/SwithPage.js
@@ -1,24 +1,18 @@
 import React from 'react'
+import styles from '../sass/skills.module.scss'
 // import { Component } from 'react'
 
 class SwithPage extends React.Component{
-     
-    constructor(props){
-        super(props);
-        this.state = {msg:true};
-        this.msg = this.msg.bind(this);
-    }
-    msg = () => {
-            this.setState(prevState => ({
-                msg: !prevState.msg
-              }))
-        }
-    
-    render(){
-        return(
-            <button onClick={this.msg}> teste bottom   {this.state.msg ? 'ON' : 'OFF'}</button>
-        );
-    }
+
+    //função trocar estado da cor do botão no nav 
+    btnPageActive = () => {
+        const [pagina, setPagina] = React.useState(true);
+        const [cor, setCor] = React.useState('linear-gradient(to left,rgb(13, 82, 151), rgb(22, 29, 97))')
+            
+            React.useEffect(()=>{
+                setCor((state)=> pagina ? 'linear-gradient(to left,rgb(33, 71, 197), rgb(96, 30, 145))' : 'linear-gradient(to left,rgb(13, 82, 151), rgb(22, 29, 97))')
+            },[pagina])
+        }   
 }
 
 export default SwithPage
