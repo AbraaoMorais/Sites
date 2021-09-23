030380f1bffb000671db735304244fe208d10ece
Abraão Morais <moraisabraao@gmail.com>
2021-09-23 00:36:35 -0300

    updated popup component

--- website_2/my-portfoliosite/components/Footer.js
+++ website_2/my-portfoliosite/components/Footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/footer.module.css"
 const footer = () =>{
     return(
         <footer className = {styles.footer}>
-            <h5>Coperight © 2021 - Abraão Morais</h5>
+            <p>Coperight © 2021 - Abraão Morais</p>
             <p>&emsp;|&emsp; Developed in Next.js</p>
             <p>&emsp;|&emsp; Status: under development..</p>
         </footer>
