5ed14ad71e4ba68b6ae75f0a7f6dcd75dd6d5c7f
Abraão Morais <moraisabraao@gmail.com>
2021-10-28 02:41:39 -0300

    added evaluation system in games cards

--- website_2/my-portfoliosite/components/Game-item.js
+++ website_2/my-portfoliosite/components/Game-item.js
@@ -1,6 +1,7 @@
 import Proptypes from 'prop-types';
 import { useState } from 'react';
 import styles from '../scss//page-projects/games.module.scss'
+import AvaliateSystem from './AvaliationSystem';
 
 const GameItem = (props) => {
 
@@ -10,23 +11,45 @@ const GameItem = (props) => {
             <div tabindex="-1" onClick={props.onOpen} className={styles.cover_wrapper}>
                
                 <figure>
-                    <img src={props.gameCoverUrl} />
+                    <img src={props.gameCoverURL} />
                     
                 </figure>
                 <div className={styles.cover_content}>
-                    <h3>{props.gameTitle}</h3>
-                    <p>{props.gameDescription}</p>
                     
-                    <button>Play now</button>       
+                    <h3>{props.gameTitle}</h3>  
+                    
+                    <p>Genre: {props.gameGenero}</p>
+                    
+                    <div>
+                        <AvaliateSystem
+                       avaliation = {props.avaliation}
+                    />
+                    </div>
+                    
+                    <p>{props.gameDescription}</p>
+                      
+                    <a href={props.gameURL} target="_blank">
+                        
+                        <button> Play now </button>  
+                    </a> 
+                    
                 </div>
                    
             </div>
+           
             <input className={styles.close_cover} type="button" value="╳" alt="close button" title="close"/>
+           
         </>
     );
 }
 
 GameItem.Proptypes = {
-    gameCoverUrl: Proptypes.string.isRequired,
+    
+    gameTitle: Proptypes.string.isRequired,
+    gameGenero: Proptypes.string.isRequired,
+    avaliation: Proptypes.number.isRequired,
+    gameDescription: Proptypes.string.isRequired,
+    gameURL: Proptypes.string.isRequired,
+    gameCoverURL: Proptypes.string.isRequired,
 }
 export default GameItem;
--- website_2/my-portfoliosite/pages/portfolio/games.jsx
+++ website_2/my-portfoliosite/pages/portfolio/games.jsx
@@ -25,20 +25,25 @@ const Web = () => {
                 {/* ============= CONTENT SECTION ===================*/}
                 <section className={games.games_wrapper}>
                     <GameItem 
-                        gameTitle="dio dino game"
-                        gameDescription="Lorem Ipsum is simply dummy text of the printing and     typesetting industry. Lorem Ipsum has been the industry's   standard dummy text ever since the 1500s, when an unknown     printer took a galley of type and scrambled it to make a    type specimen book."
-                        onOpen = {()=> {}}
-                        
-                        gameCoverUrl = "/src/covergames/dio-dino-cover.png"
+                        avaliation = {5}
+                        gameTitle="dio dino: the game"
+                        gameGenero="Platform game"
+                        gameDescription= {<>Developed in the practical class of the javascript course ES6 offered by the D.I.O (Digital Innovation One). The same is a recreation of the famous chrome dinosaur game, available when internet connection is interrupted in chrome browser.
+                            </>}
+                            
+                        gameURL="https://abraaomorais.github.io/games/dio-dino-game"
+                        gameCoverURL = "/src/covergames/dio-dino-cover.png"
                     />
                      <GameItem 
                         gameTitle="other game title"
+                        gameGenero="undefined"
+                        avaliation={3}
                         gameDescription="Lorem Ipsum is simply dummy text of the printing and     typesetting industry. Lorem Ipsum has been the industry's   standard dummy text ever since the 1500s, when an unknown     printer took a galley of type and scrambled it to make a    type specimen book."
-                        gameCoverUrl = "/src/certificados/cf1test.jpg"
+                        gameCoverURL = "/src/certificados/cf1test.jpg"
                     />
                      <GameItem 
 
-                        gameCoverUrl = "/src/covergames/dio-dino-cover.png"
+                        gameCoverURL = "/src/covergames/dio-dino-cover.png"
                     />
                 </section>
 
