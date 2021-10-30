9e777c0c394d7306085d853441e44fe18a4c702c
Abraão Morais <moraisabraao@gmail.com>
2021-10-30 18:46:51 -0300

    added prateleiras e bg-image for game page

--- website_2/my-portfoliosite/pages/portfolio/games.jsx
+++ website_2/my-portfoliosite/pages/portfolio/games.jsx
@@ -13,7 +13,7 @@ const Web = () => {
        
         <main  className = {styles.body}>
 
-            <section id="contact" className = {styles.side_right_container}>
+            <section id="gameSection" className = {styles.side_right_container + " "+ games.games_bg}>
                 
                 <TitleSubtitle 
                     
@@ -23,6 +23,34 @@ const Web = () => {
                 />
 
                 {/* ============= CONTENT SECTION ===================*/}
+                <section className={games.games_wrapper}>
+                    <GameItem 
+                        // onOpen={()=> setOverflow("hidden")}
+                        avaliation = {5}
+                        gameTitle="dio dino: the game"
+                        gameGenero="Platform game"
+                        gameDescription= {<>Developed in the practical class of the javascript course ES6 offered by the D.I.O (Digital Innovation One). The same is a recreation of the famous chrome dinosaur game, available when internet connection is interrupted in chrome browser.
+                            </>}
+                        alt="dio dino game capa"
+                        title="dio dino game"
+                            
+                        gameURL="https://abraaomorais.github.io/games/dio-dino-game"
+                        gameCoverURL = "/src/covergames/dio-dino-cover.png"
+                    />
+                     <GameItem 
+                        gameTitle="other game title"
+                        gameGenero="undefined"
+                        avaliation={3}
+                        gameDescription="Lorem Ipsum is simply dummy text of the printing and     typesetting industry. Lorem Ipsum has been the industry's   standard dummy text ever since the 1500s, when an unknown     printer took a galley of type and scrambled it to make a    type specimen book."
+                        gameCoverURL = "/src/certificados/cf1test.jpg"
+                    />
+                     <GameItem 
+
+                        gameCoverURL = "/src/covergames/dio-dino-cover.png"
+                    />
+                    
+                </section>
+                <prateleira className={games.games_prateleira}>select a game</prateleira>
                 <section className={games.games_wrapper}>
                     <GameItem 
                         avaliation = {5}
@@ -48,7 +76,8 @@ const Web = () => {
                         gameCoverURL = "/src/covergames/dio-dino-cover.png"
                     />
                 </section>
-
+                <prateleira className={games.games_prateleira}>select a game</prateleira>
+                
            </section>
         </main>
     );
